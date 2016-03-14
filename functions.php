bb28f44a17da724f5954d4c245829a9c0d5f425a
Jordan Pakrosnis <jpak@jordanpak.com>
2016-03-14 02:14:40 +0000

    Start Genesis Engine

--- functions.php
+++ functions.php
@@ -11,3 +11,11 @@ define( 'CHILD_THEME_AUTHOR_URL', 'http://jordanpak.com/' );
 define( 'CHILD_THEME_URL', 'https://github.com/JordanPak/company-front' );
 define( 'CHILD_THEME_VERSION', '1.0.0' );
 define( 'TEXT_DOMAIN', 'company-front' );
+
+
+/**
+ * Start Genesis engine
+ *
+ * @since 1.0.0
+ */
+include_once( get_template_directory() . '/lib/init.php' );
