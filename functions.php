875bb714211bb25d4e7d2af1cad6cc2febc24dd8
Jordan Pakrosnis <jpak@jordanpak.com>
2016-03-14 02:48:54 +0000

    Make it a child theme...

--- functions.php
+++ functions.php
@@ -21,117 +21,117 @@ define( 'TEXT_DOMAIN', 'company-front' );
  */
 include_once( get_template_directory() . '/lib/init.php' );
 
-
-
-/**
- * Load Assets
- *
- * @since 1.0.0
- */
-add_action( 'wp_enqueue_scripts', 'cf_load_assets' );
-function cf_load_assets() {
-
-	// Google Fonts
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:400,700,700italic', array(), CHILD_THEME_VERSION );
-
-	// FontAwesome
-	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
-
-	// Javascript
-	wp_enqueue_script( 'cf-global', get_stylesheet_directory_uri() . '/assets/js/global.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
-
-} // cf_load_assets()
-
-
-
-/**
- *
- * Theme Support
- *
- * @since 1.0.0
- *
- */
-add_theme_support( 'genesis-responsive-viewport' );
-add_theme_support( 'html5',  array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
-add_theme_support( 'genesis-after-entry-widget-area' );
-
-
-
-/**
- * Custom Body Classes
- *
- * @since 1.0.0
- * @uses /lib/body-classes.php
- */
-include_once( get_stylesheet_directory() . '/lib/body-classes.php' );
-
-
-
-/**
- * Unregister Useless WordPress/Genesis Stuff
- *
- * @since 1.0.0
- */
-
-// Remove Edit Link
-add_filter( 'edit_post_link', '__return_false' );
-
-// Unregister Unneeded Layouts and Sidebars
-unregister_sidebar( 'sidebar-alt' );
-genesis_unregister_layout( 'content-sidebar-sidebar' );
-genesis_unregister_layout( 'sidebar-sidebar-content' );
-genesis_unregister_layout( 'sidebar-content-sidebar' );
-
-// Remove Admin Menu Stuff
-function cf_remove_admin_menus(){
-	remove_menu_page( 'edit-comments.php' );
-}
-add_action( 'admin_menu', 'cf_remove_admin_menus' );
-
-// Remove Comments
-remove_action( 'genesis_after_entry', 'genesis_get_comments_template' );
-remove_action( 'genesis_comments', 'genesis_do_comments' );
-
-
-
-add_filter( 'get_the_content_more_link', 'cf_read_more_link' );
-/**
-* Customize Read More Link for Content Limit in Genesis Settings
-*
-* @author Jordan Pakrosnis
-* @since 1.0.0
-*
-* @return string
-*/
-function cf_read_more_link() {
-	return '... <a class="more-link" href="' . get_permalink() . '">Continue Reading</a>';
-}
-
-
-
-add_filter('genesis_footer_creds_text', 'cf_footer_creds');
-/**
-* Customize Footer Credits
-*
-* @since 1.0.0
-*
-* @return string
-*/
-function cf_footer_creds( $creds ) {
-	$creds = '[footer_copyright] <b>Company Front</b> &nbsp;&middot;&nbsp; ';
-	$creds .= ' Designed &amp; Developed by <a href="http://jordanpak.com" target="_BLANK">JordanPak</a>';
-	return $creds;
-}
-
-
-
-/**
- * Set the Theme Color Meta for modern browsers
- *
- * @author Jordan Pakrosnis
- * @since 1.0.0
- */
-add_action( 'genesis_meta', 'cf_meta_theme_color', 13 );
-function cf_meta_theme_color() {
-	echo '<meta name="theme-color" content="#1D1D1D">';
-}
+//
+//
+// /**
+//  * Load Assets
+//  *
+//  * @since 1.0.0
+//  */
+// add_action( 'wp_enqueue_scripts', 'cf_load_assets' );
+// function cf_load_assets() {
+//
+// 	// Google Fonts
+// 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:400,700,700italic', array(), CHILD_THEME_VERSION );
+//
+// 	// FontAwesome
+// 	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
+//
+// 	// Javascript
+// 	wp_enqueue_script( 'cf-global', get_stylesheet_directory_uri() . '/assets/js/global.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+//
+// } // cf_load_assets()
+//
+//
+//
+// /**
+//  *
+//  * Theme Support
+//  *
+//  * @since 1.0.0
+//  *
+//  */
+// add_theme_support( 'genesis-responsive-viewport' );
+// add_theme_support( 'html5',  array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+// add_theme_support( 'genesis-after-entry-widget-area' );
+//
+//
+//
+// /**
+//  * Custom Body Classes
+//  *
+//  * @since 1.0.0
+//  * @uses /lib/body-classes.php
+//  */
+// include_once( get_stylesheet_directory() . '/lib/body-classes.php' );
+//
+//
+//
+// /**
+//  * Unregister Useless WordPress/Genesis Stuff
+//  *
+//  * @since 1.0.0
+//  */
+//
+// // Remove Edit Link
+// add_filter( 'edit_post_link', '__return_false' );
+//
+// // Unregister Unneeded Layouts and Sidebars
+// unregister_sidebar( 'sidebar-alt' );
+// genesis_unregister_layout( 'content-sidebar-sidebar' );
+// genesis_unregister_layout( 'sidebar-sidebar-content' );
+// genesis_unregister_layout( 'sidebar-content-sidebar' );
+//
+// // Remove Admin Menu Stuff
+// function cf_remove_admin_menus(){
+// 	remove_menu_page( 'edit-comments.php' );
+// }
+// add_action( 'admin_menu', 'cf_remove_admin_menus' );
+//
+// // Remove Comments
+// remove_action( 'genesis_after_entry', 'genesis_get_comments_template' );
+// remove_action( 'genesis_comments', 'genesis_do_comments' );
+//
+//
+//
+// add_filter( 'get_the_content_more_link', 'cf_read_more_link' );
+// /**
+// * Customize Read More Link for Content Limit in Genesis Settings
+// *
+// * @author Jordan Pakrosnis
+// * @since 1.0.0
+// *
+// * @return string
+// */
+// function cf_read_more_link() {
+// 	return '... <a class="more-link" href="' . get_permalink() . '">Continue Reading</a>';
+// }
+//
+//
+//
+// add_filter('genesis_footer_creds_text', 'cf_footer_creds');
+// /**
+// * Customize Footer Credits
+// *
+// * @since 1.0.0
+// *
+// * @return string
+// */
+// function cf_footer_creds( $creds ) {
+// 	$creds = '[footer_copyright] <b>Company Front</b> &nbsp;&middot;&nbsp; ';
+// 	$creds .= ' Designed &amp; Developed by <a href="http://jordanpak.com" target="_BLANK">JordanPak</a>';
+// 	return $creds;
+// }
+//
+//
+//
+// /**
+//  * Set the Theme Color Meta for modern browsers
+//  *
+//  * @author Jordan Pakrosnis
+//  * @since 1.0.0
+//  */
+// add_action( 'genesis_meta', 'cf_meta_theme_color', 13 );
+// function cf_meta_theme_color() {
+// 	echo '<meta name="theme-color" content="#1D1D1D">';
+// }
