ce64247a57dfbc39fd6b964693a8666ebce129d5
Jordan Pakrosnis <jpak@jordanpak.com>
2016-03-14 02:13:38 +0000

    Define Constants

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Child Theme Constants
+ *
+ * @since 1.0.0
+ */
+define( 'CHILD_THEME_NAME', 'Company Front' );
+define( 'CHILD_THEME_AUTHOR', 'Jordan Pakrosnis' );
+define( 'CHILD_THEME_AUTHOR_URL', 'http://jordanpak.com/' );
+define( 'CHILD_THEME_URL', 'https://github.com/JordanPak/company-front' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+define( 'TEXT_DOMAIN', 'company-front' );
