b156e767244aefd91a5ced518b7f0772aec3e81f
Jordan Pakrosnis <jpak@jordanpak.com>
2016-03-14 02:18:45 +0000

    Load default assets

--- functions.php
+++ functions.php
@@ -13,9 +13,31 @@ define( 'CHILD_THEME_VERSION', '1.0.0' );
 define( 'TEXT_DOMAIN', 'company-front' );
 
 
+
 /**
  * Start Genesis engine
  *
  * @since 1.0.0
  */
 include_once( get_template_directory() . '/lib/init.php' );
+
+
+
+/**
+ * Load Assets
+ *
+ * @since 1.0.0
+ */
+add_action( 'wp_enqueue_scripts', 'cf_load_assets' );
+function cf_load_assets() {
+
+	// Google Fonts
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:400,700,700italic', array(), CHILD_THEME_VERSION );
+
+	// FontAwesome
+	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
+
+	// Javascript
+	wp_enqueue_script( 'cf-global', get_stylesheet_directory_uri() . '/assets/js/global.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+
+} // cf_load_assets()
